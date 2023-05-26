335310b31e28000b391e6bf4e11af9f3773ab78b
Swarna Shikhar Das <swarnashikhar2002@gmail.com>
2023-05-26 23:30:53 +0530

    Connection added with Mongodb

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const {connectToMongoDB} = require("./connect")
 const urlRoute = require('./routes/url')
+
+
 const app = express();
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=>console.log("Mongodb connected"))
+
+app.use(express.json());
 
 
 app.use("/url",urlRoute)
