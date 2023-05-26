118ce25a00a960de97fc2d73ae6d3717cea5613c
Swarna Shikhar Das <swarnashikhar2002@gmail.com>
2023-05-27 00:18:37 +0530

    Backend ready, have to work on UI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {connectToMongoDB} = require("./connect")
 const urlRoute = require('./routes/url')
-
+const URL = require('./models/url')
 
 const app = express();
 
@@ -10,6 +10,21 @@ connectToMongoDB('mongodb://localhost:27017/short-url')
 
 app.use(express.json());
 
+app.get('/:shortId',async(req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+shortId
+    },
+    {
+        $push:{
+            visitHistory:{
+                timestamp:Date.now()
+            }
+        }
+    }
+    );
+    res.redirect(entry.redirectURL);
+})
 
 app.use("/url",urlRoute)
 
